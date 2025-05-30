4e0c4835ecd72c39af78f956b7d383b2e6f95cde
Anuj Tiwari <anujtiwari0043@gmail.com>
2025-05-30 19:41:07 +0530

    Matches, Closest and Contains

--- 20_Seacrching_By_Dom/index.js
+++ 20_Seacrching_By_Dom/index.js
@@ -1,19 +1,46 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-        `   <div>${message}</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-    ].join('')
+// const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// const appendAlert = (message, type) => {
+//     const wrapper = document.createElement('div')
+//     wrapper.innerHTML = [
+//         `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+//         `   <div>${message}</div>`,
+//         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+//         '</div>'
+//     ].join('')
 
-    alertPlaceholder.append(wrapper)
-}
+//     alertPlaceholder.append(wrapper)
+// }
 
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-    alertTrigger.addEventListener('click', () => {
-        appendAlert('Nice, you triggered this alert message!', 'success')
-    })
-}
+// const alertTrigger = document.getElementById('liveAlertBtn')
+// if (alertTrigger) {
+//     alertTrigger.addEventListener('click', () => {
+//         appendAlert('Nice, you triggered this alert message!', 'success')
+//     })
+// }
+// getElementByclassName
+let ctitle = document.getElementsByClassName("card-title")[0]
+let c = document.getElementsByClassName("card-text")[0]
+// console.log(ctitle.style.color = "red")
+console.log(c.style.color = "blue")
+
+// getElementById
+// let d = document.getElementById("just")
+// console.log(d.style.color = "blue")
+let d = document.getElementById("justid");
+d.style.color = "Olive ";
+console.log(d.style.color);
+console.log(d)
+
+// querySelectorAll and querselector 
+let e = document.querySelectorAll(".card-title")
+console.log(e)
+e[0].style.color = "red"
+e[1].style.color = "blue"
+e[2].style.color = "green"
+
+// getElementByTagname
+console.log(document.body.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+console.log(document.getElementsByName("search"))
+
---
+++ 21_ Matches_Closest_and_Contains/index.js
@@ -0,0 +1,10 @@
+// Matches Closest and Contains
+
+let id1 = document.getElementById("id1")
+let id2 = document.getElementById("id2")
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+let sp1 = document.getElementById("p1");
+console.log(sp1.closest(".box"));
+console.log(id1.contains(sp1));
+console.log(sp2.contains(p1));
